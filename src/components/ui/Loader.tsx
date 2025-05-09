9dc7854b91872ff5ecedf7a2fce65ac1e4d8b64a
Kushwinth <kushwinthkumar0215@gmail.com>
2025-05-09 22:48:47 +0530

    landing logo updated

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import Logo from './Logo';
+// import Logo from './Logo';
 
 const Loader: React.FC = () => {
   return (
     <div className="fixed inset-0 bg-dark-900 flex items-center justify-center z-50">
       <div className="flex flex-col items-center">
         <div className="relative">
-          <Logo className="h-24 w-24 animate-spin-slow" />
+          {/* <Logo className="h-75 w-75 animate-spin-slow" />  */}
+          {/* Logo loaded from public folder */}
+          <img 
+            src="/bdtslogo.png" 
+            alt="BASEL DYNAMICS TECH Logo"
+            className="h-75 w-75 animate-spin-slow" 
+          />
           <div className="absolute inset-0 flex items-center justify-center">
-            <div className="h-16 w-16 rounded-full border-t-2 border-b-2 border-primary-500 animate-spin"></div>
+            <div className="h-60 w-60 rounded-full border-t-2 border-b-2 border-primary-500 animate-spin"></div> {/* Increased from h-16 w-16 */}
           </div>
         </div>
         <h2 className="font-heading text-2xl mt-8 neon-text text-center">
